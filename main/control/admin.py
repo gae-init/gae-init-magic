62229d8362e25e74f17fdb76bbed12f1379857ec
Panayiotis Lipiridis <lipiridis@gmail.com>
2015-03-28 15:37:15 +0100

    Minor

--- main/control/admin.py
+++ main/control/admin.py
@@ -33,7 +33,7 @@ class ConfigUpdateForm(wtf.Form):
   announcement_html = wtforms.TextAreaField(model.Config.announcement_html._verbose_name, filters=[util.strip_filter])
   announcement_type = wtforms.SelectField(model.Config.announcement_type._verbose_name, choices=[(t, t.title()) for t in model.Config.announcement_type._choices])
   anonymous_recaptcha = wtforms.BooleanField(model.Config.anonymous_recaptcha._verbose_name)
-  brand_name = wtforms.StringField(model.Config.brand_name._verbose_name, [wtforms.validators.required()], filters=[util.strip_filter])
+  brand_name = wtforms.StringField('Brand Name', [wtforms.validators.required()], filters=[util.strip_filter])
   check_unique_email = wtforms.BooleanField(model.Config.check_unique_email._verbose_name)
   email_authentication = wtforms.BooleanField(model.Config.email_authentication._verbose_name)
   feedback_email = wtforms.StringField(model.Config.feedback_email._verbose_name, [wtforms.validators.optional(), wtforms.validators.email()], filters=[util.email_filter])
@@ -94,6 +94,7 @@ class AuthUpdateForm(wtf.Form):
   vk_app_secret = wtforms.StringField(model.Config.vk_app_secret._verbose_name, filters=[util.strip_filter])
   yahoo_consumer_key = wtforms.StringField(model.Config.yahoo_consumer_key._verbose_name, filters=[util.strip_filter])
   yahoo_consumer_secret = wtforms.StringField(model.Config.yahoo_consumer_secret._verbose_name, filters=[util.strip_filter])
+  check_unique_email = wtforms.BooleanField(model.Config.check_unique_email._verbose_name)
 
 
 @app.route('/admin/auth/', methods=['GET', 'POST'])
