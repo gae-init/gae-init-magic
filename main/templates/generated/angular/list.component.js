1685997d4eee3f7218e411256bb2dff78b5172d5
Panayiotis Lipiridis <lipiridis@gmail.com>
2016-06-07 19:27:19 +0200

    Properties

--- main/templates/generated/angular/list.component.js
+++ main/templates/generated/angular/list.component.js
@@ -1,6 +1,5 @@
-angular.
-  module('{{model_db.variable_name_camel}}List').
-  component('{{model_db.variable_name_camel}}List', {
+angular.module('{{model_db.variable_name_camel}}List')
+  .component('{{model_db.variable_name_camel}}List', {
     templateUrl: 'app/{{model_db.variable_name}}/{{model_db.variable_name}}-list.template.html',
     controller: ['{{model_db.name}}',
       function {{model_db.name}}ListController({{model_db.name}}) {
--- main/templates/generated/angular/model.service.js
+++ main/templates/generated/angular/model.service.js
@@ -1,8 +1,7 @@
 'use strict';
 
-angular.
-  module('core.{{model_db.variable_name}}').
-  factory('{{model_db.name}}', ['$resource',
+angular.module('core.{{model_db.variable_name}}')
+  .factory('{{model_db.name}}', ['$resource',
     function($resource) {
       return $resource('/api/v1/admin/{{model_db.variable_name}}/', {}, {
         query: {
