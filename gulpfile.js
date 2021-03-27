2d16be96930b30eee6210d94b491b0d04923fd45
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-03-27 10:47:02 +0200

    Merge

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const gulp = require('gulp');
 const server = require('browser-sync').create();
 
 const del = require('del');
-const fs = require('fs');
 const main_bower_files = require('main-bower-files');
 const uglify = require('gulp-uglify-es').default;
 const usage = require('gulp-help-doc');
@@ -22,7 +21,8 @@ gulp.task('help', () => {
 const $ = require('gulp-load-plugins')();
 
 /*** Script ***/
-is_coffee = function(file) {
+
+is_coffee = function (file) {
   return file.path.indexOf('.coffee') > 0;
 };
 
@@ -66,6 +66,7 @@ gulp.task('script:dev', () => {
 });
 
 /*** Style ***/
+
 gulp.task('style', () => {
   return gulp
     .src(config.style)
@@ -156,23 +157,23 @@ gulp.task('pip', () => {
   );
 });
 
-gulp.task('bower', () => {
-  let cmd;
-  cmd = 'node_modules/.bin/bower install';
-  if (/^win/.test(process.platform)) {
-    cmd = cmd.replace(/\//g, '\\');
-  }
-  const start_map = [
-    {
-      cmd: cmd,
-      match: /bower.json$/,
-    },
-  ];
-  return gulp
-    .src('bower.json')
-    .pipe($.plumber())
-    .pipe($.start(start_map));
-});
+gulp.task(
+  'bower',
+  (copy_bower_sub = () => {
+    let cmd;
+    cmd = 'node_modules/.bin/bower install';
+    if (/^win/.test(process.platform)) {
+      cmd = cmd.replace(/\//g, '\\');
+    }
+    const start_map = [
+      {
+        cmd: cmd,
+        match: /bower.json$/,
+      },
+    ];
+    return gulp.src('bower.json').pipe($.plumber()).pipe($.start(start_map));
+  }),
+);
 
 gulp.task(
   'copy_bower_files',
@@ -187,13 +188,20 @@ gulp.task(
 
 gulp.task('init', gulp.parallel('pip', 'copy_bower_files'));
 
-// browserSync task
-gulp.task('browserSync', () => {
-  server.init({
-    notify: false,
-    proxy: `${config.host}:${config.port}`,
-  });
-});
+gulp.task(
+  'browserSync',
+  (python_run = () => {
+    server.init({
+      notify: false,
+      proxy: `${config.host}:${config.port}`,
+    });
+  }),
+);
+
+gulp.task(
+  'prerun',
+  gulp.series('init', gulp.parallel('ext:dev', 'script:dev', 'style:dev')),
+);
 
 /**
   Start the local server. Available options:
@@ -201,63 +209,59 @@ gulp.task('browserSync', () => {
   @arg {-o HOST} the host to start the dev_appserver.py
   @arg {-p PORT} the port to start the dev_appserver.py
   @arg {-a="..."} all following args are passed to dev_appserver.py
-  **/
+  */
 gulp.task(
   'run',
-  gulp.series(
-    'init',
-    gulp.parallel('ext:dev', 'script:dev', 'style:dev'),
-    gulp.parallel('browserSync', () => {
-      let k_iterator;
-      let options_str;
-      const argv = process.argv.slice(2);
-      const known_options = {
-        default: {
-          // eslint-disable-next-line
+  gulp.parallel('browserSync', () => {
+    let k_iterator;
+    let options_str;
+    const argv = process.argv.slice(2);
+    const known_options = {
+      default: {
+        // eslint-disable-next-line
         a: '',
-          // eslint-disable-next-line
+        // eslint-disable-next-line
         o: '',
-          // eslint-disable-next-line
+        // eslint-disable-next-line
         p: '',
-        },
-      };
-      const options = yargs(argv);
-      options_str = '-s';
-      for (k_iterator in known_options.default) {
-        if (options[k_iterator]) {
-          if (k_iterator === 'a') {
-            options_str += ` --appserver-args "${options[k_iterator]}"`;
-          } else {
-            options_str += ` -${k_iterator} ${options[k_iterator]}`;
-          }
+      },
+    };
+    const options = yargs(argv);
+    options_str = '-s';
+    for (k_iterator in known_options.default) {
+      if (options[k_iterator]) {
+        if (k_iterator === 'a') {
+          options_str += ` --appserver-args "${options[k_iterator]}"`;
+        } else {
+          options_str += ` -${k_iterator} ${options[k_iterator]}`;
         }
       }
-      if (options.p) {
-        config.port = options.p;
-      }
-      if (options.o) {
-        config.host = options.o;
-      }
-      //gulp.start('browser-sync');
-      return gulp.src('run.py').pipe(
-        $.start([
-          {
-            cmd: `python run.py ${options_str}`,
-            match: /run.py$/,
-          },
-        ]),
-      );
-    }),
-  ),
+    }
+    if (options.p) {
+      config.port = options.p;
+    }
+    if (options.o) {
+      config.host = options.o;
+    }
+    //gulp.start('browser-sync');
+    return gulp.src('run.py').pipe(
+      $.start([
+        {
+          cmd: `python run.py ${options_str}`,
+          match: /run.py$/,
+        },
+      ]),
+    );
+  }),
 );
 
 /*** Clean ***/
 
 /**
-Clean project from temporary files, generated CSS & JS and compiled Python
-files.
-@task {clean}
-**/
+  Clean project from temporary files, generated CSS & JS and
+  compiled Python files.
+  @task {clean}
+  */
 gulp.task('clean', () => {
   del('./**/*.pyc');
   del('./**/*.pyo');
@@ -282,24 +286,24 @@ gulp.task('clean:venv', () => {
 });
 
 /**
-Complete reset of project. Run "yarn install" after this procedure.
-@task {reset}
-**/
+  Complete reset of project. Run "yarn install" after this procedure.
+  @task {reset}
+  */
 gulp.task(
   'reset',
   gulp.series(
     gulp.parallel('clean', 'clean:dev', 'clean:min', 'clean:venv'),
-    () => {
+    (del_paths = () => {
       del(paths.dep.bower_components);
       return del(paths.dep.node_modules);
-    },
+    }),
   ),
 );
 
 /**
-Clear local datastore, blobstore, etc.
-@task {flush}
-**/
+  Clear local datastore, blobstore, etc.
+  @task {flush}
+  */
 gulp.task('flush', () => {
   return del(paths.temp.storage);
 });
@@ -313,19 +317,6 @@ gulp.task('yarn', () => {
     .pipe(yarn());
 });
 
-gulp.task('zip', done => {
-  if (!fs.existsSync(paths.py.lib_file)) {
-    if (fs.existsSync(paths.py.lib)) {
-      gulp
-        .src(`${paths.py.lib}/**`)
-        .pipe($.plumber())
-        .pipe($.zip('lib.zip'))
-        .pipe(gulp.dest(paths.main));
-    }
-  }
-  done();
-});
-
 /*** Watch ***/
 
 function reload(done) {
@@ -333,7 +324,7 @@ function reload(done) {
   done();
 }
 
-gulp.task('ext_watch_rebuild', callback => {
+gulp.task('ext_watch_rebuild', (callback) => {
   return gulp.parallel(
     'copy_bower_files',
     gulp.series('clean:dev', gulp.parallel('ext:dev', 'style:dev')),
@@ -341,7 +332,7 @@ gulp.task('ext_watch_rebuild', callback => {
 });
 
 gulp.task('watch', () => {
-  $.watch('requirements.txt', () => {
+  $.watch(`${paths.main}/requirements*.txt`, () => {
     return gulp.series('pip')();
   });
   $.watch('package.json', () => {
@@ -373,23 +364,16 @@ gulp.task('watch', () => {
 /*** Build ***/
 
 /**
- * Build project to prepare it for a deployment. Minify CSS & JS files and pack
- * Python dependencies into #{paths.py.lib_file}.
- * @task {build}
- * @arg {dryrun} Run all preparations but do not actually deploy
- * @arg {[other]} Other arguments are passed through to gcloud app deploy
- ***/
+  Build project to prepare it for a deployment. Minify CSS & JS files
+  and pack Python dependencies into #{paths.py.lib_file}.
+  @task {build}
+  */
 gulp.task(
   'build',
-  gulp.series(
-    'clean:min',
-    'init',
-    'ext',
-    gulp.parallel('script', 'style', 'zip'),
-  ),
+  gulp.series('clean:min', 'init', 'ext', gulp.parallel('script', 'style')),
 );
 
-/*
+/**
   Re-build project from scratch. Equivalent to "reset" and "build" tasks.
   @task {rebuild}
   */
@@ -398,49 +382,54 @@ gulp.task('rebuild', gulp.series('reset', 'build'));
 /**
   Deploy project to Google App Engine.
   @task {deploy}
-  **/
+  @arg {dryrun} Run all preparations but do not actually deploy
+  @arg {[other]} Other arguments are passed through to gcloud app deploy
+  */
 gulp.task(
   'deploy',
-  gulp.series('build', () => {
-    let dryrun;
-    let k_iterator;
-    let options_str;
-    const options = yargs(process.argv, {
-      configuration: {
-        'boolean-negation': false,
-        'camel-case-expansion': false,
-      },
-    });
-    delete options._;
-    options_str = '';
-    dryrun = '';
-    for (k_iterator in options) {
-      if (k_iterator === 'dryrun') {
-        dryrun = 'echo DRYRUN - would run: ';
-      } else {
-        if (options[k_iterator] === true) {
-          options[k_iterator] = '';
-        }
-        options_str += ` ${k_iterator.length > 1 ? '-' : ''}-${k_iterator} ${
-          options[k_iterator]
-        }`;
-      }
-    }
-    return gulp.src('run.py').pipe(
-      $.start([
-        {
-          cmd: `${dryrun}gcloud app deploy main/*.yaml${options_str}`,
-          match: /run.py$/,
+  gulp.series(
+    'build',
+    (gcloud = () => {
+      let dryrun;
+      let k_iterator;
+      let options_str;
+      const options = yargs(process.argv, {
+        configuration: {
+          'boolean-negation': false,
+          'camel-case-expansion': false,
         },
-      ]),
-    );
-  }),
+      });
+      delete options._;
+      options_str = '';
+      dryrun = '';
+      for (k_iterator in options) {
+        if (k_iterator === 'dryrun') {
+          dryrun = 'echo DRYRUN - would run: ';
+        } else {
+          if (options[k_iterator] === true) {
+            options[k_iterator] = '';
+          }
+          options_str += ` ${k_iterator.length > 1 ? '-' : ''}-${k_iterator} ${
+            options[k_iterator]
+          }`;
+        }
+      }
+      return gulp.src('run.py').pipe(
+        $.start([
+          {
+            cmd: `${dryrun}gcloud app deploy main/*.yaml${options_str}`,
+            match: /run.py$/,
+          },
+        ]),
+      );
+    }),
+  ),
 );
 
 /**
- * Start the local server, watch for changes and reload browser automatically.
- * For available options refer to "run" task.
- * @task {default}
- * @order {1}
- */
-gulp.task('default', gulp.parallel('run', 'watch'));
+  Start the local server, watch for changes and reload browser automatically.
+  For available options refer to "run" task.
+  @task {default}
+  @order {1}
+  */
+gulp.task('default', gulp.series('prerun', gulp.parallel('run', 'watch')));
